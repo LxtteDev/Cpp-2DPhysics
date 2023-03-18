07e24b034dda7c8f81f4a510e69760910a101821
LxtteDev <scott.engelbrecht2007@gmail.com>
2023-03-17 22:12:08 -0700

    Added anchor points

--- src/shapes/square.cpp
+++ src/shapes/square.cpp
@@ -3,15 +3,27 @@
 Square::Square(sf::Vector2f position, sf::Vector2f size, float rotation): Shape(sf::VertexArray(sf::Quads, 4)), position(position), size(size), rotation(rotation) {};
 
 void Square::update(Window& window) {
-    sf::Vector2f previousPosition = this->mVertices[0].position;
-    sf::Vector2f previousSize = this->mVertices[2].position - previousPosition;
+    sf::Vector2f previousPosition = this->mVertices[0].position + ( this->size * this->offset );
+    sf::Vector2f previousSize = this->mVertices[2].position + ( this->size * this->offset ) - previousPosition;
 
     if (previousPosition != this->position || previousSize != this->size) {
-        this->mVertices[0] = sf::Vector2f(this->position.x, this->position.y);
-        this->mVertices[1] = sf::Vector2f(this->position.x + this->size.x, this->position.y);
-        this->mVertices[2] = sf::Vector2f(this->position.x + this->size.x, this->position.y + this->size.y);
-        this->mVertices[3] = sf::Vector2f(this->position.x, this->position.y + this->size.y);
+        sf::Vector2f position = this->position - ( this->size * this->offset );
+
+        this->mVertices[0] = sf::Vector2f(position.x, position.y);
+        this->mVertices[1] = sf::Vector2f(position.x + this->size.x, position.y);
+        this->mVertices[2] = sf::Vector2f(position.x + this->size.x, position.y + this->size.y);
+        this->mVertices[3] = sf::Vector2f(position.x, position.y + this->size.y);
     }
 
     Shape::update(window);
+}
+
+void Square::setOrigin(sf::Vector2f origin) {
+    this->offset = origin;
+    sf::Vector2f position = this->position - ( this->size * origin );
+
+    this->mVertices[0] = sf::Vector2f(position.x, position.y);
+    this->mVertices[1] = sf::Vector2f(position.x + this->size.x, position.y);
+    this->mVertices[2] = sf::Vector2f(position.x + this->size.x, position.y + this->size.y);
+    this->mVertices[3] = sf::Vector2f(position.x, position.y + this->size.y);
 }
--- src/shapes/square.h
+++ src/shapes/square.h
@@ -1,4 +1,6 @@
+#include <iostream>
 #include "shape.h"
+#include "../math/vector.h"
 
 #ifndef SQUARE_H_
 #define SQUARE_H_
@@ -8,10 +10,14 @@ class Square : public Shape {
         Square(sf::Vector2f position, sf::Vector2f size, float rotation);
 
         void update(Window& window) override;
+        void setOrigin(sf::Vector2f origin);
 
         sf::Vector2f position;
         sf::Vector2f size;
         float rotation;
+
+    private:
+        sf::Vector2f offset;
 };
 
 #endif
