a1e9e17be9588de3b6b8f2278df686b088fdd821
LxtteDev <scott.engelbrecht2007@gmail.com>
2023-03-29 17:22:52 -0700

    Added edge clipping method

--- src/collisions/intersect.h
+++ src/collisions/intersect.h
@@ -5,7 +5,7 @@
 #ifndef INTERSECT_H_
 #define INTERSECT_H_
 
-inline bool doesIntersect(sf::VertexArray& a, sf::VertexArray& b) {
+inline bool doesIntersect(sf::VertexArray& a, sf::VertexArray& b) { // Cheaper but less accurate
     // Projections
     std::vector<sf::Vector2f> aNormals(a.getVertexCount());
     std::vector<sf::Vector2f> bNormals(b.getVertexCount());
@@ -58,9 +58,41 @@ inline bool doesIntersect(sf::VertexArray& a, sf::VertexArray& b) {
     return true;
 };
 
-template <typename T>
-std::vector<sf::Vector2<T>> edgeClip(sf::VertexArray& a, sf::VertexArray& b) {
-    
+inline std::vector<sf::Vector2f> edgeClip(sf::VertexArray& a, sf::VertexArray& b) { // More expensive but gives position
+    std::vector<sf::Vector2f> output;
+
+    for (unsigned int i = 0; i < a.getVertexCount() - 1; i++) {
+        sf::Vector2f aA = a[i].position;
+        sf::Vector2f aB = a[i + 1].position;
+
+        if (aA.x == aB.x && aA.y == aB.y) continue;
+
+        float a1 = (aB.y - aA.y);
+        float a2 = (aA.x - aB.x);
+        float aLine = a1 * aA.x + a2 * aA.y;
+
+        for (unsigned int j = 0; j < b.getVertexCount() - 1; j++) {
+            sf::Vector2f bA = b[j].position;
+            sf::Vector2f bB = b[j + 1].position;
+
+            if (bA.x == bB.x && bA.y == bB.y) continue;
+
+            float b1 = (bB.y - bA.y);
+            float b2 = (bA.x - bB.x);
+            float bLine = b1 * bA.x + b2 * bA.y;
+
+            float determinant = a1 * b2 - b1 * a2;
+
+            if (determinant != 0) {
+                float x = (b2 * aLine - a2 * bLine) / determinant;
+                float y = (a1 * bLine - b1 * aLine) / determinant;
+
+                output.push_back(sf::Vector2f(x, y));
+            }
+        }
+    }
+
+    return output;
 };
 
 #pragma once
--- src/main.cpp
+++ src/main.cpp
@@ -4,29 +4,27 @@
 #include "shapes/concave.h"
 #include "physics/collision.h"
 
-void changeColour(sf::VertexArray& array, sf::Color colour) {
-    for (unsigned int i = 0; i < array.getVertexCount(); i++) {
-        array[i].color = colour;
-    }
-}
+// void changeColour(sf::VertexArray& array, sf::Color colour) {
+//     for (unsigned int i = 0; i < array.getVertexCount(); i++) {
+//         array[i].color = colour;
+//     }
+// }
 
 int main(int, char**) {
     Window window(sf::VideoMode(1280, 720), "2D Physics engine");
 
     // Create floor
-    sf::VertexArray floorVerts(sf::LineStrip, 8);
-    floorVerts[0] = sf::Vector2f(0.0f, 570.0f);     // 0 1
-    floorVerts[1] = sf::Vector2f(320.0f, 570.0f);   // 1 0
-    floorVerts[2] = sf::Vector2f(320.0f, 620.0f);   // 2 2
-    floorVerts[3] = sf::Vector2f(640.0f, 620.0f);   // 3 4
-    floorVerts[4] = sf::Vector2f(960.0f, 670.0f);   // 4 6
-    floorVerts[5] = sf::Vector2f(1280.0f, 670.0f);  // 5 7
-    floorVerts[6] = sf::Vector2f(1280.0f, 720.0f);  // 6 5
-    floorVerts[7] = sf::Vector2f(0.0f, 720.0f);     // 7 3
+    sf::VertexArray floorVerts(sf::LineStrip, 3);
+    // floorVerts[0] = sf::Vector2f(0.0f, 570.0f);     // 0 1
+    // floorVerts[1] = sf::Vector2f(320.0f, 570.0f);   // 1 0
+    floorVerts[0] = sf::Vector2f(320.0f, 620.0f);   // 2 2
+    floorVerts[1] = sf::Vector2f(640.0f, 620.0f);   // 3 4
+    floorVerts[2] = sf::Vector2f(960.0f, 670.0f);   // 4 6
+    // floorVerts[5] = sf::Vector2f(1280.0f, 670.0f);  // 5 7
+    // floorVerts[6] = sf::Vector2f(1280.0f, 720.0f);  // 6 5
+    // floorVerts[7] = sf::Vector2f(0.0f, 720.0f);     // 7 3
 
-    Concave floor(floorVerts);
-    // Square floor(sf::Vector2f(400.0f, 470.0f), sf::Vector2f(400.0f, 200.0f));
-    // floor.rotation = 15.0f;
+    Shape floor(floorVerts);
 
     // Boxes
     Square red(sf::Vector2f(700.0f, 200.0f), sf::Vector2f(100.0f, 100.0f), sf::Color::Red);
@@ -48,6 +46,7 @@ int main(int, char**) {
     // group.addObject(pGreen);
     // group.addObject(pBlue);
     // group.addObject(pMagenta);
+
 
     while(window.open) {
         window.events();
